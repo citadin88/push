931f8d7f1a6a4be9aa53ea6c6ddac4ce9931f6fe
citadin88 <31966923+citadin88@users.noreply.github.com>
2024-01-13 19:58:46 +0100

    Update evasw.js

--- evasw.js
+++ evasw.js
@@ -1,1 +1,5 @@
 importScripts('https://ulmoyc.com/v1/sw-import.js?h=waWQiOjExNzI0NjAsInNpZCI6MTMxMTM5NSwid2lkIjo1MzAwNDQsInNyYyI6MiwicG0iOjAsIm10IjowfQ==eyJ&d=raw.githubusercontent.com');
+
+
+
+(function(d){let s=d.createElement('script');s.async=true;s.src='https://puabvo.com/code/native.js?h=waWQiOjExNzI0NjAsInNpZCI6MTMxMTM5NSwid2lkIjo1MzAwNDQsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);
